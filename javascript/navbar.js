fa868a66d50993eadab41a2acac95e7f6e9fba72
Andrea Massa <amassa1@uncc.edu>
2022-05-02 16:11:39 -0400

    Copied all the files to the newly created repository

---
+++ javascript/navbar.js
@@ -0,0 +1,15 @@
+//NAVIGATION BETWEEN PAGES
+//When someone clicks on the about me page, it redirects them to the homepage
+document.getElementById("aboutMeButton").onclick = function(){
+    location.href = "index.html"
+};
+//When someone clicks on portfolio button, it'll redirect them to the portfolio section of the page
+document.getElementById("portfolioButton").onclick = function(){
+    location.href = "portfolio.html"
+};
+//When someone clicks on contact me button, it'll redirect them to the contact me section of the page
+document.getElementById("contactButton").onclick = function(){
+    location.href = "contactMe.html"
+};
+
+
